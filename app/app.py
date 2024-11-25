29dd96949fc588a5f0a745938d254f508e4edeca
Estevo Arias García <estevo.arias@bootcamp-upgrade.com>
2024-11-25 22:34:52 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -714,39 +714,7 @@ elif page == "Test de Hipótesis":
     """,
     unsafe_allow_html=True
 )
-
-    st.markdown(
-    """
-    <div style="
-        background-color: #f9f9f9; 
-        border: 2px solid #dcdcdc; 
-        border-radius: 8px; 
-        padding: 20px; 
-        margin: 20px 0;
-        text-align: center;">
-        <p style="font-size: 18px; color: #333; margin-bottom: 15px;">
-        La fórmula del modelo de regresión lineal es:
-        </p>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
-
-    st.markdown(
-        r"""
-        <div style="
-            background-color: #f9f9f9; 
-            border: 2px solid #dcdcdc; 
-            border-radius: 8px; 
-            padding: 20px; 
-            margin: 20px 0;
-            text-align: center;">
-        $$ y = \beta_0 + \beta_1 x + \epsilon $$
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
-    
+  
     st.latex(r"y = \beta_0 + \beta_1 x + \epsilon")
 
     st.image('img/reglin.jpg', use_column_width=True)
