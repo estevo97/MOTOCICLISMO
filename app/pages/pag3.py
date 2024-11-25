8ceb37eb2f3d786d1fd92f90a31abcd31911c6cc
Estevo Arias García <estevo.arias@bootcamp-upgrade.com>
2024-11-25 19:25:28 +0100

    Update pag3.py

--- app/pages/pag3.py
+++ app/pages/pag3.py
@@ -1,3 +1,23 @@
+import streamlit as st
+import streamlit.components.v1 as components
+from streamlit_option_menu import option_menu
+import numpy as np
+import pandas as pd
+import warnings
+import base64
+from pandas.errors import SettingWithCopyWarning
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly_express as px
+from plotly.subplots import make_subplots
+import folium
+from folium.plugins import FastMarkerCluster
+from streamlit_folium import folium_static
+import xgboost as xgb
+import json
+from joblib import load
+from pycaret.regression import load_model
+
 st.markdown(
         """
         <div style="background-color: white; padding: 10px; border-radius: 5px;">
