9c0eb3b0d01f11e28b93e0c3ba03eaf0afd3128c
Estevo Arias García <estevo.arias@bootcamp-upgrade.com>
2025-02-12 18:24:38 +0100

    Update pag2.py

--- app/pages/pag2.py
+++ app/pages/pag2.py
@@ -89,7 +89,7 @@ components.iframe(url_velocidad_victorias, height=450, width=700)
 
 st.write("")
 st.markdown(
-    """    
+    """
     <iframe width="760px" height="500px" src="https://sway.cloud.microsoft/s/cbAoRYyPFehw4VbM/embed" frameborder="0" marginheight="0" marginwidth="0" max-width="100%" sandbox="allow-forms allow-modals allow-orientation-lock allow-popups allow-same-origin allow-scripts" scrolling="no" style="border: none; max-width: 100%; max-height: 100vh" allowfullscreen mozallowfullscreen msallowfullscreen webkitallowfullscreen></iframe>
     """,
     unsafe_allow_html=True
